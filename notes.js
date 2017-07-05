f3744cf67e1fedea07d00c4e88743c5fd6e3aa86
Darren Arcangel <darrenarcangel@gmail.com>
2017-07-05 12:54:17 -0700

    Updating notes.js

--- notes.js
+++ notes.js
@@ -1,5 +1,20 @@
 console.log('Starting notes.js');
 
+const fs = require('fs');
+
+var fetchNotes = () => {
+    try {
+        var notesString = fs.readFileSync('notes-data.json');
+        return JSON.parse(notesString);
+    }catch (e){
+        return [];
+    }
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
+};
 //module.exports.age = 25;
 
 // function () = '=>'
@@ -15,8 +30,24 @@ console.log('Starting notes.js');
 
 //Section 3, Lecture 13
 //addNote: addNote = addNote
+
+
+
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+//    console.log('Adding note', title, body);
+    var notes = fetchNotes();
+    var note = {
+        title,
+        body
+    };
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    if (duplicateNotes.length === 0){
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+
 };
 
 var getAll = () => {
